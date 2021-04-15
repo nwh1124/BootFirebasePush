4d27b5756728958ba41ad25798cf049921dd9c82
nwh1124 <nwh0716@gmail.com>
2021-04-16 00:39:59 +0900

    첫

---
+++ src/test/java/com/FirebaseNotificationServer/FirebaseNotificationServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.FirebaseNotificationServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirebaseNotificationServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
